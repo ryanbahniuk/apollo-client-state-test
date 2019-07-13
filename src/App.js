1e9466e1dc3d7ba9b25255fbdee3f2953d0a48f7
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2019-07-13 17:29:59 -0500

    removing unused code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ApolloClient from 'apollo-client'
 import { InMemoryCache  } from 'apollo-cache-inmemory'
 import { ApolloProvider, Query } from 'react-apollo'
 import { HttpLink } from 'apollo-link-http'
-import './App.css'
 
 const uri = 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn'
 const cache = new InMemoryCache()
